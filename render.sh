54f931e2de625223507bcf5727837cc1b099265c
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2025-04-04 15:20:51 +0200

    fix render script and add nix shell shebang

--- render.sh
+++ render.sh
@@ -1,30 +1,31 @@
-#!/bin/bash
-LTIME=`stat -c %Z ""$1".tex"`
-pdflatex ""$1".tex"
-zathura ""$1".pdf" &
+#!/usr/bin/env -S nix shell nixpkgs#texliveFull nixpkgs#zathura nixpkgs#ghostscript_headless nixpkgs#enscript --command bash
+
+LTIME=$(stat -c %Z "$1.tex")
+pdflatex "$1.tex"
+zathura "$1.pdf" &
 while true    
 do
-    ATIME=`stat -c %Z ""$1".tex"`
+  ATIME=$(stat -c %Z "$1.tex")
  
     if [[ "$ATIME" != "$LTIME" ]]
     then    
-        pdflatex -halt-on-error -interaction=nonstopmode ""$1".tex"; 
-        biber $1
-        pdflatex -halt-on-error -interaction=nonstopmode ""$1".tex"; 
+        pdflatex -halt-on-error -interaction=nonstopmode "$1.tex"; 
+        biber "$1"
+        pdflatex -halt-on-error -interaction=nonstopmode "$1.tex"; 
         echo $?
-        PDFTIME=`stat -c %Z ""$1".pdf"`
-        if [[ PDFTIME > ATIME ]] 
+        PDFTIME=$(stat -c %Z "$1.pdf")
+        if [[ "$PDFTIME" > "$ATIME" ]] 
         then
-            if ! pgrep -x "zathura" > /dev/null
+            if ! pgrep "zathura" > /dev/null
             then
-               zathura ""$1".pdf" &
+               zathura "$1.pdf" &
             fi
         else  
-            enscript ""$1".log" --output=""$1"_log.ps"
-            ps2pdf ""$1"_log.ps" ""$1"_log.pdf"
-            zathura ""$1"_log.pdf" &
+            enscript "$1.log" --output="$1_log.ps"
+            ps2pdf "$1_log.ps" "$1_log.pdf"
+            zathura "$1_log.pdf" &
         fi 
-        LTIME=`stat -c %Z ""$1".tex"`
+        LTIME=$(stat -c %Z "$1.tex")
     fi
     sleep 1
 done
