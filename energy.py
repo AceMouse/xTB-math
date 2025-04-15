c81da7083d7e980755410556fda50761aa3bdde6
AceMouse <humle000@gmail.com>
2025-04-15 11:34:32 +0200

    fix numpy inplementation of second order isotropics. nice stats

--- energy.py
+++ energy.py
@@ -10,22 +10,30 @@ C = 5
 #element_ids = np.array([C,C,C])
 #positions = np.array([[1,0,0],[0,1,0],[0,0,1]])
 rand = np.random.default_rng()
-element_ids = rand.choice(repZeff.shape[0], size=10)
-positions = rand.random((10,3))
+element_ids = rand.choice(repZeff.shape[0], size=1000)
+positions = rand.random((1000,3))
 atoms = list(zip(element_ids, positions))
- 
+def print_res(x1,x2,t1,t2,t3,label):
+    print(f"{label}:")
+    print(f"  normal  = {x1:.3f} in {t2-t1:.10f} sec")
+    print(f"  numpy   = {x2:.3f} in {t3-t2:.10f} sec")
+    print(f"  diff    = {((x1-x2)*100)/x2:.02f}%")
+    print(f"  speedup = {((t2-t1))/(t3-t2)}x")
+    
 def dist(v1, v2): #euclidean distance. 
     d = 0
     for a,b in zip(v1,v2):
         d += (a-b)**2
     d = sqrt(d)
     return d
+
 def euclidian_dist_sqr(positions):
     pos_sqr = np.broadcast_to(np.sum(positions**2, axis=-1), (positions.shape[0], positions.shape[0])) 
     pos_pairs = np.matmul(positions, positions.transpose())
     dist_sqr = pos_sqr-2*pos_pairs+pos_sqr.transpose()
     dist_sqr = dist_sqr * (dist_sqr > 0) # remove sligthly negative values so the sqrt works fine. 
     return dist_sqr
+
 def euclidian_dist(positions): 
     # res_1,2 = sqrt(sum((v1-v2)^2)) = sqrt(sum(v1^2)-2*v1.v2+sum(v2^2))
     '''
@@ -96,21 +104,22 @@ def repulsion_energy_np(element_ids, positions):
     return repE
 
 t1 = time.time()
-print(repulsion_energy(atoms))
+x1 = repulsion_energy(atoms)
 t2 = time.time()
-print(repulsion_energy_np(element_ids, positions))
+x2 = repulsion_energy_np(element_ids, positions)
 t3 = time.time()
-print("normal:", t2-t1)
-print("np:", t3-t2)
-
+print_res(x1,x2,t1,t2,t3,"repulsion")
 def get_partial_mulliken_charges(density_matrix, overlap_matrix):
     return np.sum(density_matrix*overlap_matrix, axis=-1).reshape(-1,3)
 
 def isotropic_electrostatic_and_XC_energy_second_order(atoms, charges):
+#    eta_ABs = np.zeros((element_ids.shape[0]*3,element_ids.shape[0]*3))
+#    gamma_ABs = np.zeros((element_ids.shape[0]*3,element_ids.shape[0]*3))
+#    energies = np.zeros((element_ids.shape[0]*3,element_ids.shape[0]*3))
     acc = 0
-    for A,v1 in atoms:
+    for i,(A,v1) in enumerate(atoms):
         etaA = chemicalHardness[A]
-        for B,v2 in atoms:
+        for j,(B,v2) in enumerate(atoms):
             etaB = chemicalHardness[B]
             R_AB2 = dist(v1,v2)**2
             for u in range(nShell[A]):
@@ -119,7 +128,14 @@ def isotropic_electrostatic_and_XC_energy_second_order(atoms, charges):
                     kvB = shellHardness[B][v]
                     eta_ABuv = 0.5*(etaA*(1+kuA)+etaB*(1+kvB))
                     gamma_ABuv = 1./sqrt(R_AB2+eta_ABuv**(-2))
-                    acc += charges[A][u]*charges[B][v]*gamma_ABuv
+                    e = charges[A][u]*charges[B][v]*gamma_ABuv
+                    acc += e
+#                    eta_ABs[i*3+u,j*3+v] = eta_ABuv
+#                    gamma_ABs[i*3+u,j*3+v] = gamma_ABuv
+#                    energies[i*3+u,j*3+v] = e
+#    print(eta_ABs)
+#    print(gamma_ABs)
+#    print(energies)
     acc *= 0.5
     return acc
 
@@ -129,10 +145,13 @@ def isotropic_electrostatic_and_XC_energy_second_order_np(element_ids, positions
     ks = (shellHardness[element_ids]+1).flatten()
     etas = np.broadcast_to(np.repeat(chemicalHardness[element_ids],3)*(ks), (ks.shape[0], ks.shape[0]))
     eta_ABs = (etas + etas.transpose())*0.5
-    include_shell = np.repeat(nShell[element_ids], 3) > np.repeat(np.array([[1,2,3]]),element_ids.shape[0],axis=0).flatten()
+    include_shell = np.repeat(nShell[element_ids], 3) >= np.repeat(np.array([[1,2,3]]),element_ids.shape[0],axis=0).flatten()
     include_shell = np.outer(include_shell, include_shell)
-    gamma_ABs = 1./np.sqrt(R_AB2+eta_ABs**(-2))
+    gamma_ABs = 1./np.sqrt(R_AB2+(eta_ABs**(-2)))
     energies = np.outer(charges[element_ids].flatten(), charges[element_ids].flatten())*gamma_ABs
+#    print(eta_ABs*include_shell)
+#    print(gamma_ABs*include_shell)
+#    print(energies*include_shell)
     return np.sum(energies*include_shell)*0.5
 
 def density_initial_guess(): #TODO: Make good initial guess
@@ -147,12 +166,11 @@ density_matrix = density_initial_guess()
 overlap_matrix = overlap_initial_guess()
 partial_mulliken_charges = get_partial_mulliken_charges(density_matrix, overlap_matrix)
 t1 = time.time()
-print(isotropic_electrostatic_and_XC_energy_second_order(atoms, partial_mulliken_charges))
+x1 = isotropic_electrostatic_and_XC_energy_second_order(atoms, partial_mulliken_charges)
 t2 = time.time()
-print(isotropic_electrostatic_and_XC_energy_second_order_np(element_ids, positions, partial_mulliken_charges))
+x2 = isotropic_electrostatic_and_XC_energy_second_order_np(element_ids, positions, partial_mulliken_charges)
 t3 = time.time()
-print("normal:", t2-t1)
-print("np:", t3-t2)
+print_res(x1,x2,t1,t2,t3,"isotropic electrostatic and XC energy second order")
 
 def isotropic_electrostatic_and_XC_energy_third_order(atoms, charges):
     acc = 0
@@ -232,10 +250,10 @@ def GFN2_coordination_numbers_np(element_ids, positions):
 
 
 t1 = time.time()
+x1 = 0
 for A_idx,_ in enumerate(atoms):
-    GFN2_coordination_number(A_idx, atoms)
+    x1 += GFN2_coordination_number(A_idx, atoms)
 t2 = time.time()
-GFN2_coordination_numbers_np(element_ids, positions)
+x2 = np.sum(GFN2_coordination_numbers_np(element_ids, positions))
 t3 = time.time()
-print("normal:", t2-t1)
-print("np:", t3-t2)
+print_res(x1,x2,t1,t2,t3,"coordination numbers")
