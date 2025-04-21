bdbbaa38c54566429eb3a42924e532a7005361ce
AceMouse <humle000@gmail.com>
2025-04-21 23:27:38 +0200

    include sub tex files in render script observations

--- render.sh
+++ render.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env -S nix shell nixpkgs#texliveFull nixpkgs#zathura nixpkgs#ghostscript_headless nixpkgs#enscript --command bash
 
-LTIME=$(stat -c %Z "$1.tex")
+LTIME=$(ls *.tex -Art | tail -n 1 | xargs stat -c %Z)
 pdflatex "$1.tex"
 zathura "$1.pdf" &
 while true    
 do
-  ATIME=$(stat -c %Z "$1.tex")
- 
+    ATIME=$(ls *.tex -Art | tail -n 1 | xargs stat -c %Z)
     if [[ "$ATIME" != "$LTIME" ]]
     then    
         pdflatex -halt-on-error -interaction=nonstopmode "$1.tex"; 
@@ -25,7 +24,7 @@ do
             ps2pdf "$1_log.ps" "$1_log.pdf"
             zathura "$1_log.pdf" &
         fi 
-        LTIME=$(stat -c %Z "$1.tex")
+        LTIME=$(ls *.tex -Art | tail -n 1 | xargs stat -c %Z)
     fi
     sleep 1
 done
