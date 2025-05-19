db6908bab5394957bcea9f4cc71ab10e44cafdde
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2025-05-19 14:12:35 +0200

    update xyz parser

--- xyz_reader.py
+++ xyz_reader.py
@@ -1,9 +1,27 @@
+import numpy as np
+
+element2id = {
+    "H": 0,
+    "C": 5,
+    "N": 6,
+    "O": 7
+}
+
 def parse_xyz(file):
+    element_ids = []
+    positions = []
     with open(file, "r") as f:
         f.readline()
         f.readline()
-        atoms = [
-            (e[0], [float(e[1]), float(e[2]), float(e[3])])
-            for e in (line.split() for line in f)
-        ]
-    return atoms
+
+        for line in f:
+            e = line.split()
+            element_ids.append(element2id[e[0]])
+            positions.append([float(e[1]), float(e[2]), float(e[3])])
+    element_ids = np.array(element_ids)
+    positions = np.array(positions, dtype=float)
+    return element_ids, positions
+
+element_ids, positions = parse_xyz("./caffeine.xyz")
+print(f"element_ids: {element_ids}")
+print(f"positions: {positions}")
