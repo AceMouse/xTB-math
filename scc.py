1debb89101e42f06734e24201c0fc0876a4cf11b
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2025-06-24 21:12:50 +0200

    add some wrong gpu pseudo code

--- scc.py
+++ scc.py
@@ -18,6 +18,43 @@ def electro(nbf, H0, P, dq, dqsh, atomicGam, shellGam, jmat, shift):
     scc = es + 2.0 * h * evtoau
     return es, scc
 
+# Coalesced access
+# block * block_size + thread
+# example: 0 * 256 + 2 = thread 2 of block 0
+
+# idx = block * block_size + thread
+# P[block, thread] => P[idx] (Flattened)
+
+# thread * block
+# [ ...
+#   ...
+#   ... ]
+
+# NOTE: Pseudo code for lock step parallel version of electro
+#def electro(nbf, H0, P, dq, dqsh, atomicGam, shellGam, jmat, shift):
+#    es = get_isotropic_electrostatic_energy(dq, dqsh, atomicGam, shellGam, jmat, shift)
+#
+#    h_dev = sycl_malloc(number_of_blocks * block_size)
+#
+#    ######### KERNEL ##########
+#    idx = block * blocksize + thread
+#
+#    if (thread < block):
+#        h_dev[idx] = P[idx] * H0[idx]
+#
+#    if (thread == block):
+#        h_dev[idx] = P[idx] * H0[idx] * 0.5
+#
+#    # barrier
+#    reduce(h_dev)
+#    ###########################
+#
+#    # copy hr from device to host
+#    h = sycl_move(h_dev)
+#
+#    scc = es + 2.0 * h * evtoau
+#    return es, scc
+
 
 def get_isotropic_electrostatic_energy(qat, qsh, atomicGam, shellGam, jmat, shift):
     eThird = get_third_order_energy(qat, qsh, atomicGam, shellGam)
@@ -25,9 +62,20 @@ def get_isotropic_electrostatic_energy(qat, qsh, atomicGam, shellGam, jmat, shif
     jmat_cvx = cvxopt.matrix(jmat)
     qsh_cvx = cvxopt.matrix(qsh)
     shift_cvx = cvxopt.matrix(shift)
+
+    # y := alpha * Ax + beta * y
+    # shift_cvx = 1.0 * jmat_cvx * qsh_cvx + 0.0 * shift_cvx
     cvxopt.blas.symv(jmat_cvx, qsh_cvx, shift_cvx, uplo='L', alpha=1.0, beta=0.0)
 
     return 0.5 * cvxopt.blas.dot(shift_cvx, qsh_cvx) + eThird
+
+#def symv(A, x, y, uplo='L', alpha=1.0, beta=0.0):
+#    y_dev = sycl_malloc(y)
+#    A_dev = sycl_malloc(A.flatten)
+##    ######### KERNEL ##########
+##    idx = block * blocksize + thread
+#    alpha * A_dev[idx]
+##    ###########################
 
 
 def get_third_order_energy(qat, qsh, atomicGam, shellGam):
