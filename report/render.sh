53985eb1c0b3bac4dce2b167c1ece4f831442003
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2025-07-11 15:21:58 +0200

    update latex render script to use inotify watches

--- report/render.sh
+++ report/render.sh
@@ -1,31 +1,26 @@
 #!/usr/bin/env -S nix shell nixpkgs#texliveFull nixpkgs#zathura nixpkgs#ghostscript_headless nixpkgs#enscript --command bash
 
-LTIME=$(ls *.tex -Art | tail -n 1 | xargs stat -c %Z)
-pdflatex "$1.tex"
-zathura "$1.pdf" &
-while true    
-do
-    ATIME=$(ls *.tex -Art | tail -n 1 | xargs stat -c %Z)
-    if [[ "$ATIME" != "$LTIME" ]]
-    then    
-        pdflatex -halt-on-error -interaction=nonstopmode "$1.tex"; 
-        biber "$1"
-        pdflatex -halt-on-error -interaction=nonstopmode "$1.tex"; 
-        echo $?
-        PDFTIME=$(stat -c %Z "$1.pdf")
-        if [[ "$PDFTIME" > "$ATIME" ]] 
-        then
-            if ! pgrep "zathura" > /dev/null
-            then
-               zathura "$1.pdf" &
-            fi
-        else  
-            enscript "$1.log" --output="$1_log.ps"
-            ps2pdf "$1_log.ps" "$1_log.pdf"
-            zathura "$1_log.pdf" &
-        fi 
-        LTIME=$(ls *.tex -Art | tail -n 1 | xargs stat -c %Z)
-    fi
-    sleep 1
-done
+mainfile="$1"
 
+# Initial compile
+pdflatex -halt-on-error -interaction=nonstopmode "$mainfile.tex"
+biber "$mainfile"
+pdflatex -halt-on-error -interaction=nonstopmode "$mainfile.tex"
+
+zathura "$mainfile.pdf" &
+
+# Watch for changes in .tex files in current directory and subdirectories
+while inotifywait -r -e modify,create,delete,move --exclude '\.swp$' .; do
+    echo "Change detected. Recompiling..."
+
+    if pdflatex -halt-on-error -interaction=nonstopmode "$mainfile.tex"; then
+        biber "$mainfile"
+        pdflatex -halt-on-error -interaction=nonstopmode "$mainfile.tex"
+    else
+        mainfile_log="${mainfile}_log"
+        echo "Compilation failed. Showing log..."
+        enscript "$mainfile.log" --output="$mainfile_log.ps"
+        ps2pdf "$mainfile_log.ps" "$mainfile_log.pdf"
+        zathura "$mainfile_log.pdf" &
+    fi
+done
