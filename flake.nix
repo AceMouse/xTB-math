45cd9149c1d763987d3b5d1dfe7938898321325d
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2025-08-07 13:00:18 +0200

    add cmp-impls-h2o

--- flake.nix
+++ flake.nix
@@ -49,6 +49,20 @@
         ''];
       };
 
+      xtb_test_data_h2o = builtins.derivation {
+        name = "xtb-test-data";
+        system = "x86_64-linux";
+        builder = "${pkgs.bash}/bin/bash";
+        src = ./xtb-python/data/h2o.xyz;
+        args = ["-c" ''
+          PATH=$PATH:${pkgs.coreutils}/bin
+          mkdir -p ./calls/{build_SDQH0,coordination_number,dim_basis,dtrf2,electro,form_product,get_multiints,h0scal,horizontal_shift,multipole_3d,newBasisset,olapp}
+          ${xtb}/bin/xtb $src
+          ${dftd4}/bin/dftd4 $src
+          mv calls $out
+        ''];
+      };
+
       #electro_data = let
       #  xtb = (self.packages."x86_64-linux".xtb.overrideAttrs (finalAttrs: previousAttrs: {
       #    patches = [
@@ -89,6 +103,20 @@
         program = toString (pkgs.writeShellScript "cmp-impls" ''
           PYTHONPATH=${pkgs.lib.cleanSource ./xtb-python} exec ${python}/bin/python \
             ${./xtb-python/cmp_impls.py} ${xtb_test_data}
+        '');
+      };
+
+      "cmp-impls-h2o" = let
+        python = (pkgs.python3.withPackages (python-pkgs: with python-pkgs; [
+          numpy
+          scipy
+          cvxopt
+        ]));
+      in {
+        type = "app";
+        program = toString (pkgs.writeShellScript "cmp-impls" ''
+          PYTHONPATH=${pkgs.lib.cleanSource ./xtb-python} exec ${python}/bin/python \
+            ${./xtb-python/cmp_impls.py} ${xtb_test_data_h2o}
         '');
       };
 
